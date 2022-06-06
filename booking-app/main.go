f98c1860ae3dbd6f37ad8124ca3ff29e3f08b111
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-06 21:16:13 +0530

    added data types

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,6 +3,22 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Print("Hello")
+
+	var eventName = "Nothing"
+	const tickets int = 150
+	var remainTickets int = 50
+	fmt.Println("--------------------------------------------")	
+	fmt.Printf("Welcome to %v Ease Booking app\n", eventName)
+	fmt.Println("--------------------------------------------")	
+	fmt.Printf("We have total of %v tickets and %v are still available for you.\n", tickets, remainTickets)
+	fmt.Println("Get your tickets RIGHT NOW!! to attend")
+
+	// User name input
+
+	var userName string = "XYZ"
+	var userTickets int = 10
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	
 }
 
