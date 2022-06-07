794c78f2287fc22edd19209435afcd0ea3c45442
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-07 13:58:58 +0530

    updated with slices

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,16 +7,20 @@ func main(){
 	var eventName = "Nothing"
 	const tickets int = 150
 	var remainTickets uint = 50
+	var ticketsBooking []string
+
 	fmt.Println("--------------------------------------------")	
 	fmt.Printf("Welcome to %v Ease Booking app\n", eventName)
 	fmt.Println("--------------------------------------------")	
 	fmt.Printf("We have total of %v tickets and %v are still available for you.\n", tickets, remainTickets)
 	fmt.Println("Get your tickets RIGHT NOW!! to attend")
 
+	
+	
 	// User details input
 
 	var userFirstName string
-	var userLasttName string
+	var userLastName string
 	var userEmail string
 	var userTickets uint
 
@@ -25,7 +29,7 @@ func main(){
 	fmt.Println()
 	
 	fmt.Print("Please enter your last name : ")
-	fmt.Scan(&userLasttName)
+	fmt.Scan(&userLastName)
 	fmt.Println()
 	
 	fmt.Print("Please enter your E-mail address : ")
@@ -37,11 +41,15 @@ func main(){
 	fmt.Println()
 	
 	remainTickets = remainTickets - userTickets
+	ticketsBooking = append(ticketsBooking, userFirstName + " " + userLastName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will recive a confirmation mail at %v\n ", userFirstName, userLasttName, userTickets, userEmail)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation mail at %v\n ", userFirstName, userLastName, userTickets, userEmail)
 	fmt.Println()
-	
-	fmt.Printf("%v tickets remaining for %v\n",remainTickets,eventName)
 
+	fmt.Printf("%v tickets remaining for %v",remainTickets,eventName)
+	fmt.Println()
+
+	fmt.Printf("These are all our bookings %v", ticketsBooking)
+	
 }
 
