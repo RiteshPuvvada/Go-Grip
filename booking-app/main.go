262848fc036381d3ee3c4dc0fa5e3411b5e24d6f
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-07 16:21:31 +0530

    updated with if/else

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	var eventName = "Nothing"
+	var eventName = "GOLANG"
 	const tickets int = 150
 	var remainTickets uint = 50
 	var ticketsBooking []string
@@ -42,22 +42,32 @@ func main() {
 		fmt.Scan(&userTickets)
 		fmt.Println()
 
-		remainTickets = remainTickets - userTickets
-		ticketsBooking = append(ticketsBooking, userFirstName+" "+userLastName)
+		if userTickets <= remainTickets {
+			remainTickets = remainTickets - userTickets
+			ticketsBooking = append(ticketsBooking, userFirstName+" "+userLastName)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation mail at %v\n ", userFirstName, userLastName, userTickets, userEmail)
-		fmt.Println()
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation mail at %v\n ", userFirstName, userLastName, userTickets, userEmail)
+			fmt.Println()
 
-		fmt.Printf("%v tickets remaining for %v", remainTickets, eventName)
-		fmt.Println()
+			fmt.Printf("%v tickets remaining for %v", remainTickets, eventName)
+			fmt.Println()
 
-		userFirstNames := []string{}
-		for _, booking := range ticketsBooking{
-			var names = strings.Fields(booking)
-			userFirstNames = append(userFirstNames,names[0])
+			userFirstNames := []string{}
+			for _, booking := range ticketsBooking {
+				var names = strings.Fields(booking)
+				userFirstNames = append(userFirstNames, names[0])
+			}
+
+			fmt.Printf("The first names of bookings are : %v", userFirstNames)
+			fmt.Println()
+
+			if remainTickets == 0 {
+				fmt.Println("Our conference is booked out!")
+				break
+			}
+
+		} else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainTickets, userTickets)
 		}
-
-		fmt.Printf("The first names of bookings are : %v", userFirstNames)
-		fmt.Println()
 	}
 }
