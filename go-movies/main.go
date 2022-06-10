69f038b96c08479d1f4fd69ff9828f43b3aff20b
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-10 14:29:24 +0530

    updated with routes and functions

--- go-movies/main.go
+++ go-movies/main.go
@@ -22,3 +22,22 @@ type Director struct{
 	FirstName string `json:"firstname"`
 	LastName string `json:"lastname`
 }
+
+var movies []Movie
+
+func main(){
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "145", Tittle: "Test", Director: &Director{FirstName: "J0hn", LastName: "charlie"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "146", Tittle: "Test 2", Director: &Director{FirstName: "olive", LastName: "richards"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies", createMovies).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovies).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
+	
+	fmt.Printf("Starting the server at port 3000\n")
+	log.Fatal(http.ListenAndServe(":3000",r))
+
+}
