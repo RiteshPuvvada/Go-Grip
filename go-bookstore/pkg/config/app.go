63e22bb48e46223f96a5c3d667b7b3baf60c26bb
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-11 21:37:31 +0530

    updated config: app.go

---
+++ go-bookstore/pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect(){
+	d, err := gorm.Open("mysql", "ritesh:Ritesh08082001/gobookstore?charset=utf8&paraseTime=True&loc=Local")
+	if err != nil{
+		panic(err)
+	}
+	db =d
+}
+
+func GetDB() *gorm.DB{
+	return db
+}
--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -1,12 +1,11 @@
 package routes
 
 import (
-
-	"github.com/gorilla/mux"
 	"github.com/RiteshPuvvada/Go-Grip/tree/main/go-bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
 )
 
-var RegisterBookStoreRoutes = func(router *mux.Router){
+var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
