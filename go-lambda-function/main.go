129e183127390ce79b868657614d93dffa759981
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-16 13:45:25 +0530

    functional

--- go-lambda-function/main.go
+++ go-lambda-function/main.go
@@ -5,16 +5,16 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Event struct{
+type MyEvent struct{
 	Name string `json:"what is your name?"`
 	Age int `json:"How old are you"`
 }
 
-type Response struct{
+type MyResponse struct{
 	Message string `json:"Answer"`
 }
-func HandleLambdaEvent(event Event)(Response, error){
-	return Response{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+func HandleLambdaEvent(event MyEvent)(MyResponse, error){
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
 }
 
 func main(){
