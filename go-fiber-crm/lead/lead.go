675fd08beafe81f2b5856b8a55443909283c6827
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-22 15:36:16 +0530

    added json

--- go-fiber-crm/lead/lead.go
+++ go-fiber-crm/lead/lead.go
@@ -9,10 +9,10 @@ import (
 
 type Lead struct{
 	gorm.Model
-	Name      string
-	Company   string
-	Email     string
-	Phone     int
+	Name      string     `json:"name"`
+	Company   string     `json:"company"`
+	Email     string     `json:"email"`
+	Phone     int        `json:"phone"`
 }
 
 func GetLeads(c *fiber.Ctx){
--- go-fiber-crm/main.go
+++ go-fiber-crm/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/RiteshPuvvada/Go-Grip/tree/main/go-fiber-crm/database"
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func Routes(app *fiber.App){
