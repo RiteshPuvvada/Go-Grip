de4697dec195271254e8d42d4ad4358289e730d2
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-22 15:32:17 +0530

    added lead functions

--- go-fiber-crm/lead/lead.go
+++ go-fiber-crm/lead/lead.go
@@ -14,3 +14,42 @@ type Lead struct{
 	Email     string
 	Phone     int
 }
+
+func GetLeads(c *fiber.Ctx){
+	db := database.DBConnection
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
+}
+
+func GetLead(c *fiber.Ctx){
+	id := c.Params("id")
+	db := database.DBConnection
+	var lead Lead
+	db.Find(&lead, id)
+	c.JSON(lead)
+
+}
+
+func NewLead(c *fiber.Ctx){
+	db := database.DBConnection
+	lead := new(Lead)
+	if err:=c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
+}
+
+func DeleteLead(c *fiber.Ctx){
+	id := c.Params("id")
+	db := database.DBConnection
+	var lead Lead
+	db.First(&lead,id)
+	if lead.Name == ""{
+		c.Status(500).Send("No lead found with ID")
+	}
+	db.Delete(&lead)
+	c.Send("Lead Successfully Deleted")
+}
