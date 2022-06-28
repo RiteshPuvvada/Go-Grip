8e6573fb53bbfb846de095beeb1130d8253d095c
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-28 21:41:12 +0530

    updated get handler func

--- go-fiber-mongo-hrms/main.go
+++ go-fiber-mongo-hrms/main.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"log"
 	"time"
-
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -23,10 +22,10 @@ const DbName = "fiber-hrms"
 const mongoURI = "mongodb://localhost:27017" + DbName
 
 type Employee struct{
-	ID        string 
-	Name      string 
-	Salary    float64
-	Age       float64
+	ID        string    `json:"id,omitempty" bson:"_id, omitempty"`
+	Name      string    `json:"name"`
+	Salary    float64   `json:"salary"`
+	Age       float64   `json:"age"`
 }
 
 func Connect() error{
@@ -56,7 +55,18 @@ func main(){
 	app := fiber.New()
 
 	app.Get("/employee", func(c *fiber.Ctx) error {
-		
+		query := bson.D{{}}
+
+		cursor, err := MI.Db.Collection("employees").Find(c.Context(), query)
+		if err != nil{
+			return c.Status(500).SendString(err.Error())
+		}
+		var employees []Employee =  make([]Employee, 0)
+
+		if err := cursor.All(c.Context(), &employees); err != nil{
+			return c.Status(500).SendString(err.Error())
+		}
+		return c.JSON(employees)
 	})
 	app.Post("/employee")
 	app.Put("/employee/:id")
