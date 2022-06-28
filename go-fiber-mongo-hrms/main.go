b198ee47c09b5e46dbe63af188593513b5bd0799
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-28 21:57:19 +0530

    updated post function

--- go-fiber-mongo-hrms/main.go
+++ go-fiber-mongo-hrms/main.go
@@ -68,7 +68,29 @@ func main(){
 		}
 		return c.JSON(employees)
 	})
-	app.Post("/employee")
+
+	app.Post("/employee", func(c *fiber.Ctx) error {
+		collection := MI.Db.Collection("employees")
+
+		employee := new(Employee)
+		if err := c.BodyParser(employee); err != nil{
+			return c.Status(500).SendString(err.Error())
+		}
+		employee.ID = ""
+		insertionResult, err := collection.InsertOne(c.Context(), employee)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		
+		filter := bson.D{{Key:"_id", Value: insertionResult.InsertedID}}
+		createdRecord := collection.FindOne(c.Context(), filter)
+
+		createdEmployee := &Employee{}
+		createdRecord.Decode(createdEmployee)
+		
+		return c.Status(201).JSON(createdEmployee)
+	})
+
 	app.Put("/employee/:id")
 	app.Delete("/employee/:id")
 }
