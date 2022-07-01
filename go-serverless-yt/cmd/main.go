d2a16ac623435c3d5ada82a2d0fb7a21d3a6feea
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-07-01 20:45:03 +0530

    updated

--- go-serverless-yt/cmd/main.go
+++ go-serverless-yt/cmd/main.go
@@ -7,20 +7,17 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/defaults"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-	
 )
 
-func main(){
+func main() {
 	region := os.Getenv("AWS_REGION")
 	awsSession, err := session.NewSession(&aws.Config{
-		Region : aws.String(region),
+		Region: aws.String(region),
 	})
 
-	if err != nil{
+	if err != nil {
 		return
 	}
 
@@ -30,8 +27,8 @@ func main(){
 
 const tableName = "LambdaInGoUser"
 
-func handler(req events.APIGatewayProxyRequest)(*events.APIGatewayProxyResponse, error){
-	switch req.HTTPMethod{
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
 	case "GET":
 		return handlers.GetUser(req, tableName, dynaClient)
 	case "POST":
