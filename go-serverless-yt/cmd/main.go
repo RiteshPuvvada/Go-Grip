c360129c723cde434b6a0929a3fa24d24638c268
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-07-02 09:49:00 +0530

    added api responce

--- go-serverless-yt/cmd/main.go
+++ go-serverless-yt/cmd/main.go
@@ -9,6 +9,11 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var(
+	dynaClient dynamodbiface.DynamoDBAPI
 )
 
 func main() {
